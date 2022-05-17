5286c64b5df0bbb18d4fafa160f596896604344d
chrestian <105725455+chrestian@users.noreply.github.com>
2022-05-17 22:46:44 +0900

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+//masukan username akun tumbal
+$username_tumbal="masterpro1992";
+
+//masukan password akun tumbal
+$sandi_tumbal="Turnitin@2029";
+
+
+
+
+//masukan username ig yang akan
+//di tambah follow
+$username="chrestian90";
